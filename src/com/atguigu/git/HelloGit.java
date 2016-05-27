08f52b1236c2e297928c8a9b43431460e6719393
zxx-IT <ITXXJAVAXX@163.com>
2016-05-27 22:40:41 +0800

    update 4

--- src/com/atguigu/git/HelloGit.java
+++ src/com/atguigu/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
 		System.err.println("1111111111111");
 		System.err.println("2222222222222");
 		System.err.println("3333333333333");
+		System.err.println("4444444444444");
 	}
 }
